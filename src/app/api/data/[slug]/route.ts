b3b7015719a83435be84ec9e1c2e740a61f31006
Shuvo-TD <shuvotalukder7466@gmail.com>
2025-01-05 01:14:03 +0600

    feat:(api) add api route

---
+++ src/app/api/data/[slug]/route.ts
@@ -0,0 +1,31 @@
+import { NextResponse } from 'next/server';
+import data from '@/data/data.json'; // Adjust path if needed
+
+interface Params {
+  slug: string;
+}
+
+export async function GET(request: Request, { params }: { params: Params }) {
+  const { slug } = (await params) || {};
+  if (!slug) {
+    return new NextResponse(JSON.stringify({ message: 'Product not found' }), {
+      status: 404,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+
+  const product = data.find((item) => item.slug === slug);
+
+  if (!product) {
+    return new NextResponse(JSON.stringify({ message: 'Product not found' }), {
+      status: 404,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+
+  return NextResponse.json(product);
+}
