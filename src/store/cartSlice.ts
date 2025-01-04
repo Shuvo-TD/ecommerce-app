e1c25c47b12644c43109b9487da2d48bb6dd1ae2
Shuvo-TD <shuvotalukder7466@gmail.com>
2025-01-05 01:11:40 +0600

    feat:(store) add slice

---
+++ src/store/cartSlice.ts
@@ -0,0 +1,31 @@
+import { createSlice } from '@reduxjs/toolkit';
+import type { PayloadAction } from '@reduxjs/toolkit';
+import { RootState } from './store';
+import { ProductType } from '../types';
+
+interface CartState {
+  cart: ProductType[];
+}
+
+const initialState: CartState = {
+  cart: [],
+};
+
+export const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addToCart: (state, action: PayloadAction<ProductType>) => {
+      state.cart.push(action.payload);
+    },
+    clearCart: (state) => {
+      state.cart = [];
+    },
+  },
+});
+
+export const { addToCart, clearCart } = cartSlice.actions;
+
+export const selectCart = (state: RootState) => state.cart.cart;
+
+export default cartSlice.reducer;
