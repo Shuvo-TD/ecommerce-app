9db89d0391345e2740b7aaa0d471a926a15ac3c0
Shuvo-TD <shuvotalukder7466@gmail.com>
2025-01-26 13:58:26 +0600

    feat(api): remove log

--- src/app/api/data/route.ts
+++ src/app/api/data/route.ts
@@ -2,6 +2,5 @@ import { NextResponse } from 'next/server';
 import data from '@/data/data.json';
 
 export async function GET() {
-  console.log('API data:', data);
   return NextResponse.json(data);
 }
